4a050ccd35f8bcb43d7254fa44a2e7ad94b164dc
Roms10000 <romain.richardon@outlook.fr>
2025-09-05 10:46:12 +0200

    aa

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -14,7 +14,7 @@ export default function Projects() {
             <div className='flex justify-center gap-30 pt-30'>
                 <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(255, 255, 255, 0.4)">
                     <h5 className="mb-2 text-2xl font-bold  text-white underline underline-offset-4">Site de demande de prestation</h5>
-                    <img src="../../public/site_devis.jpg"></img>
+                    <img src="/site_devis.jpg"></img>
                     <div className='flex justify-center mt-5'>
                     <a href="#" target="_blank" className="text-black bg-white  hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 cursor-pointer"><FaGlobe size={20} className="text-black cursor-pointer" /></a>
                     <a href="https://github.com/Roms10000/front_end_website_devis" target="_blank" className="text-black bg-white hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 cursor-pointer"><FaGithub size={20} className="text-black cursor-pointer" /></a>
@@ -22,7 +22,7 @@ export default function Projects() {
                 </SpotlightCard>
                 <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(255, 255, 255, 0.4)">
                     <h5 className="mb-2 text-2xl font-bold  text-white underline underline-offset-4 ">Site de demande de réservation</h5>
-                    <img src="../../public/site_reservation.jpg"></img>
+                    <img src="/site_reservation.jpg"></img>
                     <div className='flex justify-center mt-5'>
                     <a href="#" className="text-black bg-white  hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 cursor-pointer"><FaGlobe size={20} className="text-black cursor-pointer" /></a>
                     <a href="https://github.com/TPierre86/Projet-Calendrier-Reservation" target="_blank"  className="text-black bg-white  hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 cursor-pointer"><FaGithub size={20} className="text-black cursor-pointer" /></a>
